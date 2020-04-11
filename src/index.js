e531839a5f2c89c610f2b5d67eb6eaa5b74a8117
unknown <ken.brandon@aol.com>
2020-04-11 15:13:27 -0700

    mvp and some stretch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { reducer, initialState } from "./reducers";
+
+const store = createStore(reducer, initialState);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
